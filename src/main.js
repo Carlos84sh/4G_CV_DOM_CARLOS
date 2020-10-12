1d35d28e1246d891615c43a95c0efb83b33ed2b0
Carlos84sh <Cs.huelves@gmail.com>
2020-10-12 16:02:33 +0000

    actualizacion de datos

--- src/main.js
+++ src/main.js
@@ -1,16 +1,25 @@
 require('./style.css')
+    //foto de perfil
+    const divphoto = document.querySelector('#photo')
+    const img = document.createElement('IMG');
+    const src = img.src="https://placebear.com/g/200/200";
+    img.style.width='200px';
+    img.style.width='200px';
+    img.style.margin='0auto';
+    img.style.borderRadius = "150px";
 
-/**
- * Estructure sus datos de la forma que considere
- */
-const data = {
+    //Información de perfil
+    const data = {Nombre:'Carlos Sánchez Huelves',
+                    Telefono:'633362477',
+                    Correo:'cs.huelves@gmail.com'};
+    
+    const personal = document.querySelector('#personal')
 
+
+
+window.onload = function() {    
+    divphoto.appendChild(img)
+   
 }
 
-window.onload = function() {
-  /* Modifique el código en este bloque */
 
-  document.body.innerHTML = "<h1>My Resumee</h1>"
-
-  /* Fin del bloque*/
-}
